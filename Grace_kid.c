05c4b009da9f70ccae04d642dd89860b73877519
Bulliby <gwells@student.42.fr>
2017-01-01 19:55:35 +0100

    feat(master):ok

--- Grace_kid.c
+++ Grace_kid.c
@@ -1,7 +1,8 @@
 #include <fcntl.h>
 #include <stdio.h>
 #define Q(x)char*s=#x;x
-#define FT(x)int main(){FILE *file; file = fopen("Grace_kid.c", "w"); Q(fprintf(file, "#include <fcntl.h>\n#include <stdio.h>\n#define Q(x)char*s=#x;x\n#define FT(x)int main(){FILE *file; file = fopen(\"Grace_kid.c\", \"w\"); Q(%s);}\nFT(xxxxxxxx)\n/*\n\tcommentaire\n*/\n", s));}
+#define TOTO
+#define FT(x)int main(){FILE *file; file = fopen("Grace_kid.c", "w"); Q(fprintf(file, "#include <fcntl.h>\n#include <stdio.h>\n#define Q(x)char*s=#x;x\n#define TOTO\n#define FT(x)int main(){FILE *file; file = fopen(\"Grace_kid.c\", \"w\"); Q(%s);}\nFT(xxxxxxxx)\n/*\n\tcommentaire\n*/\n", s));}
 FT(xxxxxxxx)
 /*
 	commentaire
